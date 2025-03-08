6ada9d20e8bb2b76434b5f8c36e2b4662136380c
MN - TG <mntg3509@gmail.com>
2025-03-08 11:54:14 +0530

    Update route.py

--- route.py
+++ route.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("JishuBotz")
+    return web.json_response("Ente Sarasuuu")
 
 
 async def web_server():
@@ -12,12 +12,3 @@ async def web_server():
     web_app.add_routes(routes)
     return web_app
 
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @JishuBotz
-# Developer @JishuDeveloper
